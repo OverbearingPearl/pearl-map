0fab4d01d81f6ae7598f15b92179ad7c98d086cf
OverbearingPearl <OverbearingPearl@users.noreply.github.com>
2025-10-30 06:24:27 +0000

    deploy: c36c1c800bb8dfc8db11c18b69f5d0e04ed6c496

--- src/cljs/pearl_map/components/map/container.cljs
+++
@@ -1,9 +0,0 @@
-(ns pearl-map.components.map.container)
-
-(defn map-container []
-  [:div {:id "map-container"
-         :style {:width "100%"
-                 :height "100vh"
-                 :position "absolute"
-                 :top 0
-                 :left 0}}])
